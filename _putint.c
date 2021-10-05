d3bf1cfc1507f06b03bf5028284be8189d24b75a
NathyG2524 <88330679+NathyG2524@users.noreply.github.com>
2021-10-05 19:45:21 +0300

    print int

--- _putint.c
+++ _putint.c
@@ -9,5 +9,19 @@
  */
 int _putint(int c)
 {
-	return (write(1,&c, 2));
+		unsigned char my_char;
+
+	my_char = va_arg(c, int);
+	_putchar(my_char);
+	return (1);
+}
+/**
+ * print_porcentage - %
+ *
+ * Return: 1.
+ */
+int print_porcentage(void)
+{
+	_putchar('%');
+	return (1);
 }
